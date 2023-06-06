9157a5a5d942f9db5cda768df8d9e9a5acbd47fa
SlashAlexx <119014375+SlashAlexx@users.noreply.github.com>
2023-06-05 23:16:27 -0500

    Updated Test Course

--- Assets/Scripts/Hit.cs
+++ Assets/Scripts/Hit.cs
@@ -6,10 +6,10 @@ public class Hit : MonoBehaviour
 {
     public Animator anim;
     public Rigidbody2D rb;
-    bool is_active;
     public GameObject hit_line;
-    public Camera cam;
+
     Vector2 hit_direction;
+    bool is_active;
 
     void Start()
     {
@@ -19,16 +19,17 @@ public class Hit : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) || is_active)
+        if (Input.GetKeyDown(KeyCode.Mouse0) || is_active) // Left click held down
         {
             FollowMouse();
 
             is_active = true;
             hit_line.SetActive(true);
             anim.SetBool("IsActive", true);
+            GetMeterStrength();
         }
-        
-        if (Input.GetKeyUp(KeyCode.Mouse0))
+
+        if (Input.GetKeyUp(KeyCode.Mouse0)) // Left click release
         {
             anim.SetBool("IsActive", false);
             hit_line.SetActive(false);
@@ -37,13 +38,22 @@ public class Hit : MonoBehaviour
         }
     }
 
-    private void FollowMouse()
+    private void FollowMouse() // Handles ball arrow and shot direction
     {
         Vector2 mouse_position = Input.mousePosition;
         Vector2 line_position = Camera.main.WorldToScreenPoint(hit_line.transform.position);
         float angle = Mathf.Atan2(mouse_position.y -= line_position.y, mouse_position.x -= line_position.x) * Mathf.Rad2Deg;
         hit_line.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
         hit_direction = (-(Vector3)mouse_position - transform.position);
     }
 
+    private void GetMeterStrength() 
+    {
+        AnimatorStateInfo animationInfo = anim.GetCurrentAnimatorStateInfo(0);
+        AnimatorClipInfo[] animationClip = anim.GetCurrentAnimatorClipInfo(0);
+
+        float time = animationClip[0].clip.length * animationInfo.normalizedTime;
+        Debug.Log(time / animationClip[0].clip.length);
+    }
 }
