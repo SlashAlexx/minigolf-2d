1af0a65414357ee330822ecd9c37b2dbb5108740
SlashAlexx <119014375+SlashAlexx@users.noreply.github.com>
2023-06-07 01:27:33 -0500

    Finished Prototype Hit Arrow
    Hit arrow will extend with mouse distance to ball. Distance from ball also determines hit strength.

--- Assets/Scripts/Hit.cs
+++ Assets/Scripts/Hit.cs
@@ -1,59 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class Hit : MonoBehaviour
 {
-    public Animator anim;
     public Rigidbody2D rb;
-    public GameObject hit_line;
+    public GameObject hole, arrow;
 
     Vector2 hit_direction;
+    float hit_strength;
     bool is_active;
 
     void Start()
     {
         is_active = false;
-        hit_line.SetActive(false);
+        arrow.SetActive(false);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0) || is_active) // Left click held down
         {
-            FollowMouse();
+            ArrowHandler();
 
             is_active = true;
-            hit_line.SetActive(true);
-            anim.SetBool("IsActive", true);
-            GetMeterStrength();
+            arrow.SetActive(true);
         }
 
         if (Input.GetKeyUp(KeyCode.Mouse0)) // Left click release
         {
-            anim.SetBool("IsActive", false);
-            hit_line.SetActive(false);
-            rb.AddForce(hit_direction.normalized * 800);
+            arrow.SetActive(false);
+            rb.AddForce(hit_direction.normalized * hit_strength);
             is_active = false;
         }
     }
 
-    private void FollowMouse() // Handles ball arrow and shot direction
+    private void ArrowHandler() // Handles ball arrow and shot direction
     {
-        Vector2 mouse_position = Input.mousePosition;
-        Vector2 line_position = Camera.main.WorldToScreenPoint(hit_line.transform.position);
-        float angle = Mathf.Atan2(mouse_position.y -= line_position.y, mouse_position.x -= line_position.x) * Mathf.Rad2Deg;
-        hit_line.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        Vector3 mouse_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse_position.z = 0f; //sets z to 0 for 2D
 
-        hit_direction = (-(Vector3)mouse_position - transform.position);
+        hit_direction = (transform.position - (Vector3)mouse_position);
+        float arrow_length = Vector3.Magnitude(transform.position - mouse_position);
+
+        //Arrow Handling
+
+        Vector3 position = Camera.main.WorldToScreenPoint(transform.position); //Converts ball position to screen point
+        Vector3 arrow_direction = Input.mousePosition - position; // Finds distance between mouse pos and ball
+        float arrow_angle = Mathf.Atan2(arrow_direction.y, arrow_direction.x) * Mathf.Rad2Deg; //gets angle between the direction
+        transform.rotation = Quaternion.AngleAxis(arrow_angle, Vector3.forward); //Updates arrow rotation with calculated angle
+
+
+        if (arrow_length < 10)  // Arrow Scaling
+        {
+            arrow.transform.localScale = new Vector3(2 * arrow_length + 2, 4, 0);
+        }
+        
+        // Hit Strength Handling
+        hit_strength = arrow_length * 100;
+
     }
 
-    private void GetMeterStrength() 
-    {
-        AnimatorStateInfo animationInfo = anim.GetCurrentAnimatorStateInfo(0);
-        AnimatorClipInfo[] animationClip = anim.GetCurrentAnimatorClipInfo(0);
-
-        float time = animationClip[0].clip.length * animationInfo.normalizedTime;
-        Debug.Log(time / animationClip[0].clip.length);
-    }
 }
